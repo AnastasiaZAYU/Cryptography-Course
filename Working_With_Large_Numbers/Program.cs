afdc901985302e0b0de9fad8ebceb0b42c1dd33b
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2023-10-18 21:05:34 +0300

    Created project Working_With_Large_Numbers

---
+++ Working_With_Large_Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new BigNumber();
+
+            Console.ReadKey();
+        }
+    }
+}
